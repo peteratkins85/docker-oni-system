1bf83bc9d1663a1958a2b930fb0118e7c121a334
Peter Atkins <peter.atkins@eagleeye.com>
2017-05-15 13:33:39 +0100

    changed common ports and ip address in docker-compose.yml and fixed bug in boot-docker.sh file

--- unix/boot-docker.sh
+++ unix/boot-docker.sh
@@ -2,23 +2,16 @@
 
 #Create project directory
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd ~/
-
-if  [! -d "web/oniSystem" ] || "web" ; then
+if  [! -d ~/web/oniSystem/bin/console ] || [! -d ~/web ] ; then
     mkdir ~/web
+    rm -r ~/web/oniSystem
+    #Clone oni-system project
+    git clone git@github.com:peteratkins85/OniManagmentSystem.git ~/web/oniSystem
 fi
-
-#Clone oni-system project
-git clone git@github.com:peteratkins85/OniManagmentSystem.git ~/web/oniSystem
-
 
 if ! grep -q oni-system.local "/etc/hosts"; then
    echo "127.0.0.1     oni-system.local" | sudo tee -a  /etc/hosts > /dev/null
 fi
-
-cd $DIR/../
 
 mkdir composer
 
